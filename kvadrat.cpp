085eec9abc61950cde50fc44c4b3a78266619e58
yaroslav <iaroslavkurpachenko@gmail.com>
2025-08-22 16:01:47 +0300

    поменял названия на норм

---
+++ kvadrat.cpp
@@ -0,0 +1,96 @@
+
+#include <stdio.h>
+#include <math.h>
+
+//TODO: linear
+//TODO: codestyle
+void linear(double b , double c);
+
+void square(double a, double b , double c);
+
+void enteringOdds(double *a, double *b, double *c);
+
+int main() {
+    double a = ..., b, c;
+
+    enteringOdds(&a , &b , &c);
+
+    if (a == 0){
+
+        printf("Уравнение является линейным\n");
+        linear(b , c);
+
+    } else {
+
+        printf("Уравнение является квадратным\n");
+        square(a , b , c);
+    }
+
+    return 0;
+}
+
+/*
+main()
+{
+    a,b,c,x1,x2, nRoots;
+    // Input();
+    // Solve();
+    // Output();
+}
+
+*/
+
+void linear(double b , double c) {
+
+    double x = 0;
+
+    if (b == 0) {
+        if (c == 0) {
+            printf("Бесконечное множество решений");
+
+        } else {
+            printf("Решений нет");
+        }
+    } else {
+
+        x = -b / c;
+        printf("Решением является: %lf", x);
+    }
+
+}
+
+
+void square(double a, double b, double c) {
+
+    double x1 = 0;
+    double x2 = 0;
+
+    double discriminant = b*b - (4*a*c);
+
+    if (discriminant > 0) {
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+
+        printf("Первый корень: x1 = %lf\n" , x1);
+        printf("Второй корень: x2 = %lf\n" , x2);
+    } else if (discriminant == 0) {
+        x1 = -b / (2*a);
+        printf("Решение: x = %lf\n" , x1);
+    } else {
+        printf("Решений нет");
+    }
+
+}
+
+void enteringOdds(double *a, double *b, double *c){
+    printf("Уравнения вида ax^2 + bx + c = 0\n");
+
+    printf("Введите коэффициент а:");
+    scanf("%lf" , a);
+
+    printf("Введите коэффициент b:");
+    scanf("%lf" , b);
+    //TODO: int success_elements = ...
+    printf("Введите коэффициент c:");
+    scanf("%lf" , c);
+}
